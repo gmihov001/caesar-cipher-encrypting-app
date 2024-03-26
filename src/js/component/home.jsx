532858cbcf34efc54441e3300e60795bd13719ac
George Mihov <53583283+gmihov001@users.noreply.github.com>
2024-03-26 16:39:15 +0000

    Complete version w/ encrypt & decrypt, + text validation

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [encryptInput, setEncryptInput] = useState("");
+	const [decryptInput, setDecryptInput] = useState("");
+	const alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+
+	const encrypt = (str) => {
+		if (str.length < 2 || str.includes(" ")) alert("Text must contain: a dot, more than 2 characters, no whitespaces")
+		else {
+			let offsetter = 5;
+			let multiplier = 3;
+			let newString = "";
+			for (let letter of str) {
+				newString += alphabet[(alphabet.indexOf(letter) + offsetter) % 26];
+			}
+			newString += `.${multiplier}`;
+			newString += offsetter * multiplier;
+			console.log("Encrypted: ", newString);
+		}
+	};
+
+	const decrypt = (str) => {
+		console.log("str.length: ", str.length);
+		console.log("str.includes: ", str.includes(" "));
+		if (str.length < 2 || str.includes(" ") || !str.includes(".")) alert("Text must contain: a dot, more than 2 characters, no whitespaces")
+		else {
+			let offsetter = 0;
+			let multiplier = 1;
+			let decypheredString = "";
+			let splitStr = str.split(".");
+			multiplier = splitStr[1].charAt(0);
+			offsetter = splitStr[1].substring(1) / multiplier;
+			for (let letter of splitStr[0]) {
+				decypheredString += alphabet[(alphabet.indexOf(letter) - offsetter) % 26];
+			}
+			console.log("Decrypted: ", decypheredString);
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App text-center">
+			<h1>Caesar's Cipher</h1>
+			<h3>Encrypt your messages</h3>
+			<div className="input-group w-50 mx-auto">
+				<input
+					className="form-control"
+					placeholder="Enter text to encrypt"
+					value={encryptInput}
+					onChange={(e) => setEncryptInput(e.target.value)}
+				/>
+				<button className="btn btn-outline-warning btn-lg" onClick={() => encrypt(encryptInput)}>Encrypt</button>
+			</div>
+			<div className="input-group w-50 mx-auto">
+				<input
+					className="form-control"
+					placeholder="Enter text to decrypt"
+					value={decryptInput}
+					onChange={(e) => setDecryptInput(e.target.value)}
+				/>
+				<button className="btn btn-outline-success btn-lg" onClick={() => decrypt(decryptInput)}>Decrypt</button>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
